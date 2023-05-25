d533fb024d668ad16dd8d4e9cb258dd361ddbf91
Anastasiya Sulimova <breadkvass@gmail.com>
2023-05-26 00:22:06 +0300

    upd webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
@@ -12,5 +17,31 @@ module.exports = {
         port: 8080,
         open: true
       },
-}
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
+          },
+          {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+          },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader'
+            }]
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+          }),
+          new CleanWebpackPlugin(),
+          new MiniCssExtractPlugin()
+      ]
+};
 
