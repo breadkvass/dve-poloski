3ca27f1fd35350083bc9dcd1c86e8e177b36c228
Anastasiya Sulimova <breadkvass@gmail.com>
2023-12-28 16:08:26 +0300

    final touches

--- webpack.config.js
+++ webpack.config.js
@@ -6,42 +6,44 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'main.js',
-            publicPath: '',
-      },
-      mode: 'development',
-      devServer: {
-        static: path.resolve(__dirname, './dist'),
-        compress: true,
-        port: 8080,
-        open: true
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: '/node_modules/'
+        publicPath: '',
+    },
+    mode: 'development',
+    devServer: {
+      static: path.resolve(__dirname, './dist'),
+      compress: true,
+      port: 8080,
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: '/node_modules/'
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
           },
-          {
-            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-            type: 'asset/resource'
-          },
-          {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, {
-              loader: 'css-loader'
-            }]
-          }
-        ]
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-          }),
-          new CleanWebpackPlugin(),
-          new MiniCssExtractPlugin()
+          'postcss-loader']
+        }
       ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin()
+    ]
 };
 
